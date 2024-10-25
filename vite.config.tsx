f1e6dac92962973b911840cc8102c31c6ab204f1
Omar Rios <omarrx14@gmail.com>
2024-10-24 22:56:41 -0700

    Update vite.config.tsx

--- vite.config.tsx
+++ vite.config.tsx
@@ -1,12 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path';
+import { defineConfig } from 'vite';
+import vercel from 'vite-plugin-vercel';
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
+  server: {
+    port: process.env.PORT as unknown as number,
   },
+  plugins: [vercel()],
 });
