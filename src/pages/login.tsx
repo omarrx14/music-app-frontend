382d969dc1fb71c95a3f0262d81139104ac6ecfb
Omar Rios <omarrx14@gmail.com>
2024-10-27 20:24:48 -0700

    Update login.tsx

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -17,7 +17,11 @@ export default function Login() {
                 "https://music-gen-demo-omars-projects-b5a3697e.vercel.app/api/users/token",
                 `username=${email}&password=${password}`,
                 {
-                    headers: { "Content-Type": "application/json" }, // Header Content-Type
+                    headers: {
+                        "Content-Type": "application/x-www-form-urlencoded",
+                        "Authorization": `Bearer ${localStorage.getItem("token") || ""}`,
+                    },
+                    withCredentials: true, // Permite el uso de credenciales si el backend las requiere
                 }
             );
             localStorage.setItem("token", response.data.access_token);
