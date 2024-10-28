b00469e52f7de60bc07277ab024830d1d86965de
Omar Rios <omarrx14@gmail.com>
2024-10-27 22:56:18 -0700

    fix

--- src/utils/auth.tsx
+++ src/utils/auth.tsx
@@ -30,7 +30,7 @@ export default function Auth() {
                 localStorage.setItem("token", response.data.access_token);
                 navigate("/dashboard");
             } else {
-                await axios.post("https://music-gen-demo-omars-projects.vercel.app/api/users/signup/", {
+                await axios.post("https://music-gen-demo-omars-projects.vercel.app/", {
                     username,
                     email,
                     password,
